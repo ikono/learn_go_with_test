8231bd75c015985244c8065c436e976904025907
HiroyaOnoki <onoki.h@gmail.com>
2020-08-04 20:22:45 +0900

    array and slice

--- 4_array_slice/sum.go
+++ 4_array_slice/sum.go
@@ -1,10 +1,39 @@
 package main
 
 // Sum ...
-func Sum(numbers [5]int) int {
+func Sum(numbers []int) int {
 	sum := 0
-	for i := 0; i < 5; i++ {
-		sum += numbers[i]
+	for _, number := range numbers {
+		sum += number
 	}
 	return sum
+}
+
+// SumAll ...
+func SumAll(numbersToSum ...[]int) []int {
+	// lengthOfNumbers := len(numbersToSum)
+	// sums = make([]int, lengthOfNumbers)
+
+	// for i, numbers := range numbersToSum {
+	// 	sums[i] = Sum(numbers)
+	// }
+	var sums []int
+	for _, numbers := range numbersToSum {
+		sums = append(sums, Sum(numbers))
+	}
+	return sums
+}
+
+// SumAllTails ...
+func SumAllTails(numbersToSum ...[]int) []int {
+	var sums []int
+	for _, numbers := range numbersToSum {
+		if len(numbers) == 0 {
+			sums = append(sums, 0)
+		} else {
+			tail := numbers[1:]
+			sums = append(sums, Sum(tail))
+		}
+	}
+	return sums
 }
--- 4_array_slice/sum_test.go
+++ 4_array_slice/sum_test.go
@@ -1,14 +1,62 @@
 package main
 
-import "testing"
+import (
+	"reflect"
+	"testing"
+)
 
 func TestSum(t *testing.T) {
-	numbers := [5]int{1, 2, 3, 4, 5}
+	t.Run("5 numbers", func(t *testing.T) {
+		numbers := []int{1, 2, 3, 4, 5}
 
-	got := Sum(numbers)
-	want := 15
+		got := Sum(numbers)
+		want := 15
 
-	if got != want {
-		t.Errorf("got is %d, want is %d. given %v", got, want, numbers)
+		if got != want {
+			t.Errorf("got is %d, want is %d. given %v", got, want, numbers)
+		}
+	})
+
+	t.Run("any numbers", func(t *testing.T) {
+		numbers := []int{1, 2, 3}
+		got := Sum(numbers)
+		want := 6
+		if got != want {
+			t.Errorf("got is %d, want is %d. numbers is %v", got, want, numbers)
+		}
+	})
+}
+
+func TestSumAll(t *testing.T) {
+	got := SumAll([]int{1, 2}, []int{0, 9})
+	want := []int{3, 9}
+	// want := "bob"
+
+	if !reflect.DeepEqual(got, want) {
+		t.Errorf("got is %v, want is %v", got, want)
 	}
+}
+
+func TestSumAllTails(t *testing.T) {
+
+	checkSums := func(t *testing.T, got, want []int) {
+		t.Helper()
+		if !reflect.DeepEqual(got, want) {
+			t.Errorf("got is %v, want is %v.", got, want)
+		}
+	}
+
+	t.Run("make sums of some slice", func(t *testing.T) {
+		got := SumAllTails([]int{1, 2}, []int{0, 9})
+		want := []int{2, 9}
+
+		checkSums(t, got, want)
+	})
+
+	t.Run("sum empty slice", func(t *testing.T) {
+		got := SumAllTails([]int{}, []int{3, 4, 5})
+		want := []int{0, 9}
+
+		checkSums(t, got, want)
+	})
 }
